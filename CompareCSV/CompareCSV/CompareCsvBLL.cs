d8d1ba535b8d091501e49245ff7cdf13da75b46d
Vignesh <warvick46@gmail.com>
2018-10-19 00:16:57 +0800

    comparecsv

---
+++ CompareCSV/CompareCSV/CompareCsvBLL.cs
@@ -0,0 +1,17 @@
+﻿
+using System.IO;
+using System.Linq;
+
+namespace CompareCSV
+{
+    public class CompareCsv
+    {
+        public static bool IsEqual(string filePath1, string filePath2)
+        {
+            var actual = File.ReadLines(filePath1);
+            var expected = File.ReadLines(filePath2);
+            var response = actual.SequenceEqual(expected);
+            return response;
+        }
+    }
+}
